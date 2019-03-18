0696f07f4cd60645151fb56a7220d3f8de9785f4
Hugo Ojvind Francois <hugo.ojvind.francois@gmail.com>
2019-03-18 22:12:44 +0100

    Few things: - Add the directory has main maven project that get all ejb-jar from all component and package it into a war file - Add configuration for the TomEE server

--- participantpool/src/main/java/fr/polytech/polydiploma/participantpool/Distinction.java
+++
@@ -1,8 +0,0 @@
-package fr.polytech.polydiploma.participantpool;
-
-public enum Distinction {
-    MAJOR,
-    HONOR,
-    HODOR,
-    HARRY
-}
--- participantpool/src/main/java/fr/polytech/polydiploma/participantpool/Main.java
+++
@@ -1,7 +0,0 @@
-package fr.polytech.polydiploma.participantpool;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("I'm participant pool");
-    }
-}
